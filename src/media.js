6d7bc80035866dec3b913987f791054c2846d3cc
Kavyashree-BK <kavyashree.bk05@gmail.com>
2026-09-02 22:05:39 +0530

    added data

--- src/media.js
+++ src/media.js
@@ -1,9 +1,11 @@
 // manifest of user media stored in /public/media or added via the in-app uploader (temporary)
 // Edit this list to include your own files (filenames under public/media)
 const initialMedia = [
-  { id: 'img-1', type: 'image', src: '/media/us.jpg', title: 'Us' },
-  { id: 'img-2', type: 'image', src: '/media/HemsBirthday.jpg', title: 'A Day to Remember' },
-  { id: 'img-3', type: 'image', src: '/media/Yard.jpg', title: 'First Pic Together' },
+  { id: 'img-1', type: 'image', src: '/media/CubbonParkWide.jpg', title: 'There was before you , There was During you' },
+  { id: 'img-2', type: 'image', src: '/media/us.jpg', title: 'Us' },
+  { id: 'img-3', type: 'image', src: '/media/HemsBirthday.jpg', title: 'A Day to Remember' },
+  { id: 'img-4', type: 'image', src: '/media/CubBook.jpg', title: 'Remainders of HIM❤️' },
+  { id: 'img-5', type: 'image', src: '/media/Yard.jpg', title: 'First Pic Together' },
   { id: 'vid-1', type: 'video', src: '/media/clip1.mp4', title: 'Our moment' },
   { id: 'aud-1', type: 'audio', src: '/media/song1.mp3', title: 'Birthday song' }
 ]
