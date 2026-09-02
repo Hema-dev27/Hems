ad31276920e5fc748434e6559b8cec84fdbda1d6
Kavyashree-BK <kavyashree.bk05@gmail.com>
2026-09-02 22:19:02 +0530

    Added videos

--- src/media.js
+++ src/media.js
@@ -3,10 +3,12 @@
 const initialMedia = [
   { id: 'img-1', type: 'image', src: '/media/CubbonParkWide.jpg', title: 'There was before you , There was During you' },
   { id: 'img-2', type: 'image', src: '/media/us.jpg', title: 'Us' },
-  { id: 'img-3', type: 'image', src: '/media/HemsBirthday.jpg', title: 'A Day to Remember' },
-  { id: 'img-4', type: 'image', src: '/media/CubBook.jpg', title: 'Remainders of HIM❤️' },
-  { id: 'img-5', type: 'image', src: '/media/Yard.jpg', title: 'First Pic Together' },
-  { id: 'vid-1', type: 'video', src: '/media/clip1.mp4', title: 'Our moment' },
+  { id: 'img-3', type: 'image', src: '/media/Babyy.jpg', title: 'The Cutest guy i have Ever Seen in my LIFE😘' },
+  { id: 'img-4', type: 'image', src: '/media/HemsBirthday.jpg', title: 'A Day to Remember' },
+  { id: 'img-5', type: 'image', src: '/media/CubBook.jpg', title: 'Remainders of HIM❤️' },
+  { id: 'img-6', type: 'image', src: '/media/Yard.jpg', title: 'First Pic Together' },
+  { id: 'vid-1', type: 'video', src: '/media/badminton.mp4', title: '😘🔥🔥🔥🔥' },
+  { id: 'img-7', type: 'image', src: '/media/MiraClib.jpg', title: 'My Cute Little Baby' },
   { id: 'aud-1', type: 'audio', src: '/media/song1.mp3', title: 'Birthday song' }
 ]
 
